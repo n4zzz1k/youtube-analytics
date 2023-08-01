6687502d2adccd2a4a1a03ea06349739b94eab3d
n4zzz1k <n4zzz1k@gmail.com>
2023-08-01 08:23:56 +0500

    Initial

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,3 @@
-# src/channel.py
-
 import requests
 
 class Channel:
@@ -7,24 +5,30 @@ class Channel:
         self.channel_id = channel_id
         self.api_key = 'AIzaSyBuuxluP5Z8a_M7xb-yUKSu6cxzWKGVius'
 
-    def print_info(self):
+        # Вызов метода для заполнения атрибутов объекта реальными данными
+        self.fetch_channel_info()
+
+    def fetch_channel_info(self):
         url = f'https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={self.channel_id}&key={self.api_key}'
         response = requests.get(url)
         data = response.json()
 
         if 'items' in data:
-            channel_info = data['items'][0]
-            title = channel_info['snippet']['title']
-            description = channel_info['snippet']['description']
-            view_count = channel_info['statistics']['viewCount']
-            subscriber_count = channel_info['statistics']['subscriberCount']
-            video_count = channel_info['statistics']['videoCount']
+            channel_info = data['items'][0]['snippet']
+            statistics = data['items'][0]['statistics']
 
-            print(f"Title: {title}")
-            print(f"Description: {description}")
-            print(f"View Count: {view_count}")
-            print(f"Subscriber Count: {subscriber_count}")
-            print(f"Video Count: {video_count}")
+            self.title = channel_info['title']
+            self.description = channel_info['description']
+            self.url = f'https://www.youtube.com/channel/{self.channel_id}'
+            self.subscriber_count = statistics['subscriberCount']
+            self.video_count = statistics['videoCount']
+            self.view_count = statistics['viewCount']
         else:
-            print("Channel not found or API key is invalid.")
+            raise ValueError("Channel not found or API key is invalid.")
 
+    def print_info(self):
+        print(f"Title: {self.title}")
+        print(f"Description: {self.description}")
+        print(f"View Count: {self.view_count}")
+        print(f"Subscriber Count: {self.subscriber_count}")
+        print(f"Video Count: {self.video_count}")

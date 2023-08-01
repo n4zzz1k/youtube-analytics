36e84ba0a4ab5393d91178ca5d97a4c59c653705
n4zzz1k <n4zzz1k@gmail.com>
2023-08-01 08:08:22 +0500

    Initial

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,30 @@
+# src/channel.py
 
+import requests
 
 class Channel:
-    """Класс для ютуб-канала"""
+    def __init__(self, channel_id):
+        self.channel_id = channel_id
+        self.api_key = 'AIzaSyBuuxluP5Z8a_M7xb-yUKSu6cxzWKGVius'
 
-    def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+    def print_info(self):
+        url = f'https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={self.channel_id}&key={self.api_key}'
+        response = requests.get(url)
+        data = response.json()
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        if 'items' in data:
+            channel_info = data['items'][0]
+            title = channel_info['snippet']['title']
+            description = channel_info['snippet']['description']
+            view_count = channel_info['statistics']['viewCount']
+            subscriber_count = channel_info['statistics']['subscriberCount']
+            video_count = channel_info['statistics']['videoCount']
+
+            print(f"Title: {title}")
+            print(f"Description: {description}")
+            print(f"View Count: {view_count}")
+            print(f"Subscriber Count: {subscriber_count}")
+            print(f"Video Count: {video_count}")
+        else:
+            print("Channel not found or API key is invalid.")
+
